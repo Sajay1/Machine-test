7e1b6d4cfcfb1084f43a59fe80e96342cdc260a9
Sajay1 <sajaysuresh307@gmail.com>
2025-05-16 15:44:06 +0530

    updates

--- server/models/Menu.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const MenuSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String
-});
-
-module.exports = mongoose.model('Menu', MenuSchema);
