433b93113d678f7ffefa832b7f58ed55620d6c2c
Sajay1 <sajaysuresh307@gmail.com>
2025-05-21 08:36:14 +0530

    upadtes

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/menu-item');
+mongoose.connect('mongodb://localhost:27017/menu-app');
 
 const db = mongoose.connection;
 
