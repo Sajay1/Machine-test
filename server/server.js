ec80b04f83e658d1e95a198c834b095dcdb15574
Sajay1 <sajaysuresh307@gmail.com>
2025-05-21 08:42:32 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,8 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 app.use('/api/menus', require('./routes/menu'));
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
